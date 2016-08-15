890607b9bab93e835e8e676653c6941d8ab0b8e5
Jack Chung <jackg825@gmail.com>
2016-08-15 13:48:50 +0800

    Connect to MongoDB Implement Create class to save "Book" to database

---
+++ src/main/scala/Book.scala
@@ -0,0 +1,2 @@
+case class Book (id : Int, name : String, publisher : String, author : String, isbn10 : String, isbn13 : String,
+                 lang : String, version : Double, pages : Int, desc : String)
---
+++ src/main/scala/MongoFactory.scala
@@ -0,0 +1,11 @@
+import com.mongodb.casbah.MongoCollection
+import com.mongodb.casbah.MongoConnection
+
+object MongoFactory {
+  private val SERVER = "localhost"
+  private val PORT   = 27017
+  private val DATABASE = "library"
+  private val COLLECTION = "books"
+  val connection = MongoConnection(SERVER)
+  val collection = connection(DATABASE)(COLLECTION)
+}
