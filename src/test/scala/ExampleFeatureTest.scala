492e82e6ca68e05d7c6257a5bc7ac9d54d406314
Jack Chung <jackg825@gmail.com>
2016-08-15 15:26:32 +0800

    Implement Testcase : Create book and save to database

--- src/test/scala/ExampleFeatureTest.scala
+++ src/test/scala/ExampleFeatureTest.scala
@@ -6,4 +6,22 @@ class ExampleFeatureTest extends FeatureTest {
 
   override val server = new EmbeddedHttpServer(new ExampleServer)
 
+  "Create Book for Post" in {
+    server.httpPost(
+      path = "/create",
+      postBody =
+        """
+          {
+            "name" : "Introduction to Algorithms",
+            "author" : "Thomas H. Cormen",
+            "isbn10" : "0262033844",
+            "isbn13" : "978-0262033848",
+            "desc" : "Algorithms"
+          }
+        """,
+      andExpect = Ok,
+      withBody = "Book name : Introduction to Algorithms\nBook author : Thomas H. Cormen\n" +
+        "Book ISBN-10 : 0262033844\nBook ISBN-13 : 978-0262033848\nBook desc : Algorithms")
+  }
+
 }
